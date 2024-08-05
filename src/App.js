529420e68795607ab3127d38ecf49f8a5070e0df
Muhammad Farooq Qaiser <farooqqaiser679@gmail.com>
2024-08-05 21:10:13 +0500

    gave default data so that the data of a defult city is shown when the user have not yet searched anything

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./components/search/Search";
 import CurrentWeather from "./components/current weather/CurrentWeather";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import Forecast from "./components/forecast/Forecast";
 
 function App() {
+  const [defaultcurrentWeather, setdefaultCurrentWeather] = useState(null);
+  const [defaultforecast, setdefaultForecast] = useState(null);
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const defaultLat = 33.6007;
+  const defaultLon = 73.0679;
+
+  useEffect(() => {
+    const defaultCurrentWeatherApi = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${defaultLat}&lon=${defaultLon}&appid=${WEATHER_API_KEY}&units=metric`
+    ).then((response) => response.json());
+
+    const defaultForecastApi = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${defaultLat}&lon=${defaultLon}&appid=${WEATHER_API_KEY}&units=metric`
+    ).then((response) => response.json());
+
+    Promise.all([defaultCurrentWeatherApi, defaultForecastApi])
+      .then((response) => {
+        setdefaultCurrentWeather(response[0]);
+        setdefaultForecast(response[1]);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   const handleSearchOnChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -23,7 +44,6 @@ function App() {
         setForecast(await response[1]);
       })
       .catch((error) => console.error(error));
-    console.log(currentWeather);
   };
 
   return (
@@ -35,7 +55,6 @@ function App() {
         {currentWeather ? (
           <>
             <CurrentWeather
-              // date={currentWeather.list[0].dt_txt}
               city={currentWeather.city.name}
               weather={currentWeather.list[0].weather[0].main}
               icon={currentWeather.list[0].weather[0].icon}
@@ -47,7 +66,24 @@ function App() {
             />
           </>
         ) : (
-          <></>
+          <>
+            {defaultcurrentWeather ? (
+              <>
+                <CurrentWeather
+                  city={defaultcurrentWeather.city.name}
+                  weather={defaultcurrentWeather.list[0].weather[0].main}
+                  icon={defaultcurrentWeather.list[0].weather[0].icon}
+                  temperature={defaultcurrentWeather.list[0].main.temp}
+                  feelsLike={defaultcurrentWeather.list[0].main.feels_like}
+                  wind={defaultcurrentWeather.list[0].wind.speed}
+                  humidity={defaultcurrentWeather.list[0].main.humidity}
+                  pressure={defaultcurrentWeather.list[0].main.pressure}
+                />
+              </>
+            ) : (
+              <></>
+            )}
+          </>
         )}
       </div>
       <div className="w-5/6">
@@ -56,7 +92,15 @@ function App() {
             <Forecast data={forecast} />
           </>
         ) : (
-          <></>
+          <>
+            {defaultforecast ? (
+              <>
+                <Forecast data={defaultforecast} />
+              </>
+            ) : (
+              <></>
+            )}
+          </>
         )}
       </div>
     </div>
